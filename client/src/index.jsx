85cd708f3e2270cfe5260c910c5248c65c50d489
rfduong <ronnyduong@gmail.com>
2021-03-29 22:33:01 -0700

    Added server and placeholder function to transform input

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,16 +5,40 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      userInput: '',
+      revUserInput: '',
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.reverseText = this.reverseText.bind(this);
+  }
+
+  // placeholder transform function, should translate into another language later
+  reverseText() {
+    const { userInput } = this.state;
+    const revUserInput = [...userInput];
+    for (let i = 0; i < revUserInput.length/2; i++) {
+      let temp = revUserInput[i];
+      revUserInput[i] = revUserInput[revUserInput.length-i-1];
+      revUserInput[revUserInput.length-i-1] = temp;
+    }
+    console.log(revUserInput);
+    this.setState({ revUserInput });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
   }
 
   render() {
+    const { userInput, revUserInput } = this.state;
     return (
       <div>
         <h2>Voice Translation</h2>
         <form>
-          <textarea value=""></textarea>
+          <textarea id="userInput" onChange={this.handleChange}></textarea>
+          <textarea value={userInput === '' ? 'Translation' : revUserInput} disabled></textarea>
         </form>
       </div>
     );

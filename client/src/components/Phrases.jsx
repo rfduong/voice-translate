c128134725ba8c5dfcf6446540cfd473eb59169e
rfduong <ronnyduong@gmail.com>
2021-04-01 12:24:58 -0700

    Changed phrases from class to functional component

--- client/src/components/Phrases.jsx
+++ client/src/components/Phrases.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
 
-class Phrases extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-  render() {
-    const { commonPhrases, changeInput } = this.props;
-    return (
-      <div id="common-phrases">
-        <h3><span>Common phrases</span></h3>
-        <div id="phrase-container">
-          {commonPhrases.map((phrase, index) => {
-            return (
-              <div className="phrase" key={phrase._id}>
-                <div className="phrase-number"><span>{`${index + 1}`}</span></div>
-                <div className="phrase-text"><span onClick={() => changeInput(phrase.userInput)}>{phrase.userInput}</span></div>
-              </div>
-            );})}
-        </div>
-      </div>
-    );
-  }
-}
+const Phrases = ({ commonPhrases, changeInput }) => (
+  <div id="common-phrases">
+    <h3><span>Common phrases</span></h3>
+      <div id="phrase-container">
+        {commonPhrases.map((phrase, index) => {
+        return (
+          <div className="phrase" key={phrase._id}>
+            <div className="phrase-number"><span>{`${index + 1}`}</span></div>
+            <div className="phrase-text"><span onClick={() => changeInput(phrase.userInput)}>{phrase.userInput}</span></div>
+          </div>
+        );})}
+    </div>
+  </div>
+);
 
 export default Phrases;
